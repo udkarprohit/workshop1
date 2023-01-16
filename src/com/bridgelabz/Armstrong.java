caa1c80ce52c0e370b0147e444f8fa6e540ac6cd
Prohit <udkarprohit@gmail.com>
2023-01-16 22:36:56 +0530

    Added: The number is armstrong or not

---
+++ src/com/bridgelabz/Armstrong.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class Armstrong {
+
+    public static void main(String[] args) {
+
+        int originalNum, digit, cubeSum = 0, num;
+
+        System.out.println("Enter the number:");
+        Scanner sc = new Scanner(System.in);
+        num = sc.nextInt();
+        //assigning num into a originalNum variable
+        originalNum = num;
+        while (num != 0) {
+            digit = num % 10;
+            cubeSum += Math.pow(digit, 3);
+            //removes the last digit
+            num /= 10;
+        }
+
+        if (cubeSum == originalNum)
+            System.out.println(originalNum + " is an Armstrong number");
+        else
+            System.out.println(originalNum + " is not an Armstrong number");
+    }
+}
